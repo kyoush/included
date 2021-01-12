8553f688346a16f857dec7ed9369ca5617fd4222
Kyoshiro Kamura <reallove1970@gmail.com>
2021-01-12 18:08:28 +0900

    changed xlabel_frequency

--- xlabel_freq.m
+++ xlabel_freq.m
@@ -5,8 +5,9 @@ xl = [20 20000];
 if length(varargin) == 1
     xl = cell2mat(varargin(1));
 end
-
-xlim([xl])
+xlim(xl)
 xticks([20 50 100 200 500 1000 2000 5000 10000 20000])
 xticklabels({'20', '50', '100', '200', '500', '1k', '2k', '5k', '10k', '20k'})
+grid on
+xlabel('Frequency [Hz]')
 end
